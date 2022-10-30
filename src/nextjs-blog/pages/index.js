83f1d166c8d2bccf41e1e52ac94eca87a3481a8c
Tyler Inari <github@al.tyler.jp>
2022-10-29 20:33:48 -0700

    npx @next/codemod new-link .

--- src/nextjs-blog/pages/index.js
+++ src/nextjs-blog/pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
 export default function Home({ allPostsData }) {
     return (
@@ -11,10 +11,6 @@ export default function Home({ allPostsData }) {
             </Head>
             <section className={utilStyles.headingMd}>
                 <p>[Your Self Introduction]</p>
-                <p>
-                    (This is a sample website - you’ll be building a site like this on{' '}
-                    <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-                </p>
             </section>
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -31,14 +27,14 @@ export default function Home({ allPostsData }) {
                 </ul>
             </section>
         </Layout>
-    );
+    )
 }
 
 export async function getStaticProps() {
-    const allPostsData = getSortedPostsData();
+    const allPostsData = getSortedPostsData()
     return {
         props: {
-            allPostsData,
-        },
-    };
+            allPostsData
+        }
+    }
 }
